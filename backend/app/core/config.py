8e5731b518d70c06138ab3e2fde74ee71d81f2c6
samuel <samuel.fonseca@colegiomontehelena.edu.co>
2025-09-15 16:26:11 -0500

    Include routes blocked

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,6 +4,6 @@ class Settings:
     PROJECT_NAME: str = "Chatbot API"
     ALLOWED_ORIGINS: List[str] = [
         "http://localhost:3000",
-        "https://mhbp-p.vercel.app/"
+        "https://mhbp-p.vercel.app"
     ]
 settings = Settings()
