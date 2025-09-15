7414ea5b379f7423f5731a53ab016d0be7a483a4
samuel <samuel.fonseca@colegiomontehelena.edu.co>
2025-09-15 16:20:06 -0500

    Conection deploy routes

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,8 +3,7 @@ from typing import List
 class Settings:
     PROJECT_NAME: str = "Chatbot API"
     ALLOWED_ORIGINS: List[str] = [
-        "http://localhost:3000"#,
-#"https://cbot-lemon.vercel.app",
-#"https://cbot-ui.vercel.app",
+        "http://localhost:3000",
+        "https://mhbp-p.vercel.app/"
     ]
 settings = Settings()
